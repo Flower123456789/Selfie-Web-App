e69aef81d878340fe1f9cbe076f61de73cd49f93
Flower123456789 <82616100+Flower123456789@users.noreply.github.com>
2021-08-31 16:30:47 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 var SpeechRecognition = window.webkitSpeechRecognition;
 
-var recognition = new SpeechRecognition;
+var recognition = new SpeechRecognition();
 
 function start(){
 
@@ -17,19 +17,28 @@ recognition.onresult = function(event) {
     var Content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = Content;
      console.log(Content);
+
+     if(Content == "take my selfie"){
+         console.log("taking my selfie ___");
      speak();
+     }
 }
 
 function speak(){
 
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking Your Selfie In 5 Seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function(){
+        take_snapshot();
+
+    }  , 5000)
 }
 
 Webcam.set({
@@ -42,3 +51,19 @@ Webcam.set({
 
 camera = document.getElementById("camera");
 
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id = "selfie_image" src = "'+data_uri+'">';
+
+    });
+}
+
+
+function save(){
+
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
